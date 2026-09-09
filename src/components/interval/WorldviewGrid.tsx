b0e480f54fa1b6c2fa5d7eba7b247e6b1eca1ec2
esheagren <esheagren1995@gmail.com>
2026-09-09 14:53:12 -0400

    Combine energy and climate and introduce institutions and governance

--- src/components/interval/WorldviewGrid.tsx
+++ src/components/interval/WorldviewGrid.tsx
@@ -4,10 +4,10 @@ const topics = [
   { name: 'Economics & trade', examples: 'Work, wealth & exchange', icon: <><path d="M6 29h25M8 29V19h5v10m3 0V13h5v16m3 0V7h5v22M7 11l8-5"/></> },
   { name: 'Global development', examples: 'Poverty, education & opportunity', icon: <><path d="M29 23a13 13 0 1 1-16-17M5 18h12M9 27h14M18 5c-8 7-8 19 0 26M18 31c3-3 5-7 6-11M20 15 31 4m-8 0h8v8"/></> },
   { name: 'China & global power', examples: 'Industry, influence & growth', icon: <><circle cx="18" cy="18" r="13"/><ellipse cx="18" cy="18" rx="6" ry="13"/><path d="M6 13h24M6 23h24"/><circle cx="24" cy="14" r="3" className="worldview-icon-dot"/></> },
-  { name: 'Energy & resources', examples: 'Power, fuels & materials', icon: <><path d="m21 4-12 17h9l-3 11 12-17h-9z"/></> },
+  { name: 'Energy & climate', examples: 'Power, resources & the environment', icon: <><path d="m21 4-12 17h9l-3 11 12-17h-9z"/></> },
   { name: 'Population & demography', examples: 'Births, aging & migration', icon: <><circle cx="18" cy="10" r="4"/><path d="M11 30v-7a7 7 0 0 1 14 0v7M6 15a3 3 0 1 1 4-4M3 28v-6a5 5 0 0 1 5-5m22-2a3 3 0 1 0-4-4m7 17v-6a5 5 0 0 0-5-5"/></> },
   { name: 'Cities & urbanism', examples: 'Housing, transport & density', icon: <><path d="M4 31h28M7 31V15h10v16m0 0V5h12v26M10 20h4m-4 5h4M21 10h4m-4 6h4m-4 6h4"/></> },
-  { name: 'Climate & environment', examples: 'Land, oceans & ecosystems', icon: <><path d="M8 25C2 10 17 7 29 6c1 13-3 24-15 22M7 31 24 13M14 24v-9m0 9h9"/></> },
+  { name: 'Institutions & governance', examples: 'Governments, laws & public services', icon: <><path d="m4 12 14-8 14 8H4Zm3 4v12m7-12v12m8-12v12m7-12v12M5 28h26M3 32h30"/></> },
 ];
 
 export function WorldviewGrid() {
